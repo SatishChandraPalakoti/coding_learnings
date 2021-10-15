07852464e4e07d294c5462d13fedd66d1e2864ec
Satish Chandra Palakoti <palakoti1244@hotmail.com>
2021-10-15 18:14:33 +0000

    Finding the second max is completed

--- SecondMax.js
+++ SecondMax.js
@@ -1,19 +1,19 @@
-let list=[9,3,4,6,2]
+let list=[2,3,4,16,2]
 
 if(list.length==1)
   return list[0]
 if(list.length==2)
   return Math.min(list[0],list[1])
 else {
-  if(firstItem==undefined && secondItem==undefined){
-    firstItem=list[0]
-    secondItem=list[1]
-  }
-  for(let i=2;i<list.length;i++){
-    firstItemAlter=Math.max(firstItem,list[i])
-    if(firstItemAlter!=firstItem){
-      secondItem=firstItem
+  console.log("Inside the else block") 
+    firstItem=list[0]>list[1]?list[0]:list[1]
+    secondItem=list[0]<list[1]?list[0]:list[1]
+  
+  for(let i=2;i<list.length;i++){ 
+    secondItem=Math.max(secondItem,list[i])
+    if(secondItem>firstItem){
+       [firstItem,secondItem]=[secondItem,firstItem]
     }
-    
-  }
+   }  
+    console.log("second largest is", secondItem); 
 }
