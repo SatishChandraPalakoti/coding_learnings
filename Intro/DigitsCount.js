e1955e496fbba36fc890b0a269a3a4e4d4524897
Satish Chandra Palakoti <palakoti1244@hotmail.com>
2022-03-29 02:59:15 +0000

    Fixes with no count in function arguments

--- Intro/DigitsCount.js
+++ Intro/DigitsCount.js
@@ -1,16 +1,18 @@
 //find occurrence of a digit in a number(recursion)
 
-let occurrence = function(number, digit, count=0){
+let occurrence = function(number, digit){
   
    if(number==0){
-      return count+=digit==number?1:0
+      return digit==number?1:0
     }else if (number < 10 && number > 0){
-      return count+=number==digit?1:0
+      return number==digit?1:0
     }else{
+     
       let extractedNumber = number % 10
-    count+= extractedNumber == digit?1:0
-      return occurrence(Math.floor(number/10), digit, count)
+      let count= extractedNumber == digit?1:0
+      return count+occurrence(Math.floor(number/10), digit)
     } 
   }
    
-  console.log(occurrence(333333454, 4))
+  console.log(occurrence(333333454,3))
+ 
--- Intro/VowelFinder.js
+++ Intro/VowelFinder.js
@@ -1,23 +1,25 @@
 // Find Vowels in a string (Recursive)
 
-let vowelFinder = function(String ,count=0){
+let vowelFinder = function(String){
     if(String.length==0)
-      return count+=0
+      return 0
     else {
      let character = String.charAt(String.length-1)
-       if (character=='a'||character=='e'||character=='i'||character=='o'||character=='u')
-        count+=1
+       if (character=='a'||character=='e'||character=='i'||character=='o'||character=='u'){ 
+        let count=1
         String = String.substr(0,String.length-1)
-        return vowelFinder(String,count)
-       
+        return count+vowelFinder(String)
+       }else{
+         String = String.substr(0,String.length-1)
+         return vowelFinder(String)
+       }
    }
 }
 
 console.log(vowelFinder("alphabet"))
 
 // Find Vowels in a string (Iterative)
-
-// Find Vowels in a string
+ 
 
 let vowelFinderIter = function(String ,count=0){
      for(var iter=0; iter<String.length; iter++){ 
