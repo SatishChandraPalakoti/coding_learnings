8c0ecdd7d088f77c0253aa9ad247b03c141fd5d5
Satish Chandra Palakoti <palakoti1244@hotmail.com>
2022-04-03 21:51:58 +0000

    Extracting the arrays

--- Intro/GroupingArray.js
+++ Intro/GroupingArray.js
@@ -1,20 +1,34 @@
 // Given an array of ints, is it possible to divide the ints into two groups, so that the sum of the two groups is the same, with these constraints: all the values that are multiple of 5 must be in one group, and all the values that are a multiple of 3 (and not a multiple of 5) must be in the other. (No loops needed.)
 
-
+let subSolution2 = function(inArr, index, left, right, leftArr,rightArr){
+  // console.log("Left arr", leftArr)
+  // console.log("Right arr", rightArr)
+  if(inArr.length==index){ 
+    if(left==right){
+    console.log("Left->", leftArr.split('')) 
+    console.log("Right->", rightArr.split('')) 
+    }
+    return left==right
+    }else{   
+      return subSolution2(inArr, index+1, inArr[index]+left, right, leftArr+(inArr[index]).toString(), rightArr) || subSolution2(inArr, index+1, left, inArr[index]+right, leftArr, rightArr+(inArr[index]).toString())
+       
+    }
+}
 let solution  = function(inputArr){
-  return subSolution(inputArr,0,0,0)
+  // return subSolution(inputArr,0,0,0)
+  return subSolution2(inputArr,0,0,0,"","")
 }
 
 let subSolution = function(inArr, index, left,right) {
   if(inArr.length==index)
-    {  console.log("Index is", index, left , right)
+    { 
       return left==right}
     else{ 
     return subSolution(inArr, index+1, left+inArr[index], right) || subSolution(inArr, index+1, left, right+inArr[index])
   }
 }
 
-console.log("-->", solution([2,1,2]))
+console.log("-->", solution([2,4,2]))
 
 // let splitter = (input, part1, part2, position)=>{
 //     if(input.length!=0){ 
@@ -39,4 +53,6 @@ console.log("-->", solution([2,1,2]))
 //         return false
 //     } 
 // }
+
+
 
